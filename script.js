4f1a5ce7657ae2cebf2dfc4f4ee2da114a48aa43
Izuchukwu Stephen Azubuike <zubix4all@hotmail.com>
2020-03-21 20:41:02 +0100

    JavaScript Task One

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let name = "Izuchukwu Stephen Azubuike"
+
+let courses = ["HTML","CSS","JavaScript"]
+
+    console.log (name,courses)
+
+for (let m = 1; m <= 200; m++)
+{
+    if (m % 2 == 0){
+
+        console.log(m)
+    }       
+      
+}
+
+
+
